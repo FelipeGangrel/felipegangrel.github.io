c9025cb192d28125c7ba3ec0a004552d1ee2b8ce
Felipe Gangrel <felipefrog@gmail.com>
2018-10-23 16:56:02 -0300

    update

--- src/js/store/state.js
+++ src/js/store/state.js
@@ -38,7 +38,7 @@ const files = [
             
             <span class="const">const</span> <span class="name">frontend</span> <span class="definer">=</span> [ <br />
                 ${tab}<span class="string">'HTML5 + CSS3'</span>, <span class="comment">// + LESS e SASS </span> <br />
-                ${tab}<span class="string">'Ionic Framework'</span>, <span class="comment">// + Apps simples e rápidos </span> <br />
+                ${tab}<span class="string">'Ionic Framework'</span>, <span class="comment">// Apps simples e rápidos </span> <br />
                 ${tab}<span class="string">'JavaScript'</span>, <span class="comment">// Nos sabores Vanilha ES6 e também com JQuery </span> <br />
                 ${tab}<span class="string">'Angular'</span>, <span class="comment">// Experiência em desenvolvimento mobile </span> <br />
                 ${tab}<span class="string">'VueJS'</span>, <span class="comment">// Meu framework para componentização de view preferido ${heart} </span> <br />
