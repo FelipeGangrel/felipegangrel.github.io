5b34dc7b30e902a36da2382eab2989c0c1de112a
Felipe Gangrel <felipefrog@gmail.com>
2019-01-28 20:29:54 -0200

    Atualizando currículo

--- src/js/store/state.js
+++ src/js/store/state.js
@@ -16,6 +16,7 @@ const files = [
             </span><br />
             <span class="const">const</span> <span class="name">name</span> <span class="definer">=</span> <span class="string">'Felipe \<Gangrel\> Pires de Oliveira'</span>;<br />
             <span class="const">const</span> <span class="name">email</span> <span class="definer">=</span> <span class="string">'<a href="mailto:felipefrog@gmail.com">felipefrog@gmail.com</a>'</span>;<br />
+            <span class="const">const</span> <span class="name">telefone</span> <span class="definer">=</span> <span class="string">'24 99253 8145'</span>;<br />
             <span class="const">const</span> <span class="name">twitter</span> <span class="definer">=</span> <span class="string link">'<a target="_blank" href="https://twitter.com/felipegangrel">https://twitter.com/felipegangrel</a>'</span>;<br />
             <span class="const">const</span> <span class="name">tranposCo</span> <span class="definer">=</span> <span class="string link">'<a target="_blank" href="https://trampos.co/olveirafelipe">https://trampos.co/olveirafelipe</a>'</span>;<br />
             <span class="const">const</span> <span class="name">linkedIn</span> <span class="definer">=</span> <span class="string link">'<a target="_blank" href="https://www.linkedin.com/in/felipegangrel/">https://www.linkedin.com/in/felipegangrel/</a>'</span>;<br />
@@ -26,6 +27,23 @@ const files = [
         filename: 'skills.js',
         content: `
 
+            <span class="const">const</span> <span class="name">apps</span> <span class="definer">=</span> [ <br />
+                ${tab}<span class="string">'React Native'</span>, <span class="comment">// Trabalhando em um projeto </span> <br />
+                ${tab}<span class="string">'Flutter'</span>, <span class="comment">// De curioso a apaixonado </span> <br />
+                ${tab}<span class="string">'Ionic Framework'</span>, <span class="comment">// Apps simples e rápidos </span> <br />
+            ];<br />
+
+            <br />
+
+            <span class="const">const</span> <span class="name">frontend</span> <span class="definer">=</span> [ <br />
+                ${tab}<span class="string">'HTML5 + CSS3'</span>, <span class="comment">// + LESS e SASS </span> <br />
+                ${tab}<span class="string">'JavaScript'</span>, <span class="comment">// Nos sabores Vanilha ES6 e também com JQuery </span> <br />
+                ${tab}<span class="string">'VueJS'</span>, <span class="comment">// Meu framework para componentização de view preferido ${heart} </span> <br />
+                ${tab}<span class="string">'Angular'</span>, <span class="comment">// Experiência em desenvolvimento mobile </span> <br />
+            ];<br />
+
+            <br />
+
             <span class="const">const</span> <span class="name">backend</span> <span class="definer">=</span> [ <br />
                 ${tab}<span class="string">'PHP'</span>, <span class="comment">// PHP Orientado a objetos + MVC</span> <br />
                 ${tab}<span class="string">'Laravel e Doctrine ORM'</span>, <span class="comment">// Não tem como não amar esta dupla</span> <br />
@@ -34,16 +52,6 @@ const files = [
                 ${tab}<span class="string">'NodeJS'</span>, <span class="comment">// Dando meus primeiros passos</span> <br />
             ];<br />
             
-            <br />
-            
-            <span class="const">const</span> <span class="name">frontend</span> <span class="definer">=</span> [ <br />
-                ${tab}<span class="string">'HTML5 + CSS3'</span>, <span class="comment">// + LESS e SASS </span> <br />
-                ${tab}<span class="string">'Ionic Framework'</span>, <span class="comment">// Apps simples e rápidos </span> <br />
-                ${tab}<span class="string">'JavaScript'</span>, <span class="comment">// Nos sabores Vanilha ES6 e também com JQuery </span> <br />
-                ${tab}<span class="string">'Angular'</span>, <span class="comment">// Experiência em desenvolvimento mobile </span> <br />
-                ${tab}<span class="string">'VueJS'</span>, <span class="comment">// Meu framework para componentização de view preferido ${heart} </span> <br />
-            ];<br />
-
             <br />
 
             <span class="const">const</span> <span class="name">tools</span> <span class="definer">=</span> [ <br />
@@ -70,6 +78,14 @@ const files = [
             <span class="comment">
                 // Emprego atual
             </span> <br />
+
+            <span class="name">jobs</span><span class="method">.add</span>(</span><span class="new">new</span> <span class="class">Job</span>(<span class="string">'Desenvolvedor Web + Apps full stack'</span>, { <br />
+                ${tab}<span class="definer">ano :</span> 2018,<br />
+                ${tab}<span class="definer">instituicao :</span> <span class="string">'Sivis Tecnologia'</span>,<br />
+                ${tab}<span class="definer">local :</span> <span class="string">'Volta Redonda - RJ'</span>,<br />
+            }));<br />
+
+            <br />
 
             <span class="name">jobs</span><span class="method">.add</span>(</span><span class="new">new</span> <span class="class">Job</span>(<span class="string">'Desenvolvedor Web full stack'</span>, { <br />
                 ${tab}<span class="definer">ano :</span> 2015,<br />
